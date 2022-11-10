a26b4e0d62e0e7cc60c4b60e489fe65317603b3a
Erkan <12980@coderacademy.edu.au>
2022-11-10 16:24:26 +1100

    added crud to exercises_controller

--- controllers/exercises_controller.py
+++ controllers/exercises_controller.py
@@ -39,6 +39,7 @@ def get_all_exercises():
     # Data in JSON format. 
     return ExerciseSchema(many=True).dump(exercises)
 
+
 # Creating a route to delete an exercise from the database.
 @exercises_bp.route('/<int:id>/', methods=['DELETE'])
 # Checking for sign in token.
@@ -48,7 +49,6 @@ def delete_exercise(id):
     authorize()
     # Creating statement to check Exercise model for the id put into endpoint.
     stmt = db.select(Exercise).filter_by(id=id)
-
     # Placing statement into database to look for a single object and storing it in exercise.
     exercise = db.session.scalar(stmt)
     # Checking if exercise exists with the given id in the route given.
@@ -58,4 +58,69 @@ def delete_exercise(id):
         return {'message': f'Exercise {exercise.name} deleted successfully'}
     else:
         return {'error': f'Exercise not found with the given id {id}'}, 404
+
+# Retrieving a specific exercise from the id number inputted in our end point.
+@exercises_bp.route('/<int:id>/')
+@jwt_required()
+def get_one_exercise(id):
+    stmt = db.select(Exercise).filter_by(id=id)
+    print(stmt)
+    exercise = db.session.scalar(stmt)
+    # Checking if exercise exists with the given id in the route given.
+    # Exclude password also.
+    if exercise:
+        return ExerciseSchema().dump(exercise)
+    else:
+        return {'error': f'Exercise not found with the given id {id}'}, 404
+
+
+# POST creation of a workout, only admins allowed. 
+@exercises_bp.route('/add-workout/', methods=['POST'])
+@jwt_required()
+def create_exercise():
+    # Authorization added so only admins can create workouts to library.
+    authorize()
+    # Create a new Exercise model instance
+    try:
+        # We are sending the request through json format else it won't be sent any other way.
+        # We can increase security by ensuring we use POST method and json body only. This prevents any SQL injection
+        exercise = Exercise(
+            name = request.json['name'],
+            muscle_group_id = request.json['muscle_group_id'],
+            exercise_equipment_id = request.json['exercise_equipment_id']
+        )    
+        # Add and commit exercise to DB
+        db.session.add(exercise)
+        db.session.commit()
+        # Respond to client excluding the password from the client
+        return ExerciseSchema().dump(exercise), 201
+    except IntegrityError:
+        return {"error" : "exercise already created"}, 409
+
+
+# Updating the inputted exercise with PUT or PATCH methods.
+@exercises_bp.route('/<int:id>/', methods=['PUT', 'PATCH'])
+@jwt_required()
+def update_exercise(id):
+    # Authorization checks if exercise is admin.
+    authorize()
+    stmt = db.select(Exercise).filter_by(id=id)
+    exercise = db.session.scalar(stmt)
+    # Checking if exercise exists with the given id in the route given.
+    if exercise:
+        exercise.name = request.json.get('name') or exercise.name
+        exercise.muscle_group_id = request.json.get('muscle_group_id') or exercise.muscle_group_id
+        exercise.exercise_equipment_id = request.json.get('exercise_equipment_id') or exercise.exercise_equipment_id
+        
+        db.session.commit()
+        return ExerciseSchema().dump(exercise)
+    else:
+        return {'error': f'Exercise not found with the given id {id}'}, 404  
+
+@exercises_bp.route('/muscle-group/<int:muscle>/')
+def get_muscle_group(muscle):
+    stmt = db.select(Exercise).filter_by(muscle_group_id = muscle)
+    muscle_group = db.session.scalars(stmt)
+    return ExerciseSchema(many=True).dump(muscle_group)
+
 
